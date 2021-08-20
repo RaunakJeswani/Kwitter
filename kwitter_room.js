b719d06825ca085566d5ccc83d5f4e3d754f8a71
RaunakJeswani <86308886+RaunakJeswani@users.noreply.github.com>
2021-08-20 05:00:16 -0700

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -10,6 +10,18 @@
  // Initialize Firebase
  firebase.initializeApp(firebaseConfig);
 
+ user_name = localStorage.getItem("user_name");
+ document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+ function addRoom() {
+     room_name = document.getElementById("room_name").value;
+     firebase.database().ref("/").child(room_name).update({
+         purpose: "adding room name"
+     });
+     localStorage.setItem("room_name", room_name);
+     window.location = "kwitter_page.html";
+ }
+
  function getData() {
      firebase.database().ref("/").on('value', function (snapshot) {
          document.getElementById("output").innerHTML = "";
@@ -17,7 +29,9 @@
              childKey = childSnapshot.key;
              Room_names = childKey;
              //Start code
-
+             console.log("Room Name - " + Room_names);
+             row = "<div class='room_name' id=" + room_names + " onclick='redirectToRommName(this.id)'>#" + room_names + "</div> <hr>";
+             document.getElementById("output").innerHTML += row;
              //End code
          });
      });
